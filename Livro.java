7c5f9a349369ed61f54975f2f7671f0eb6522e40
JoseOscarJr <53666786+JoseOscarJr@users.noreply.github.com>
2019-09-04 19:45:19 -0300

    Add files via upload

---
+++ Livro.java
@@ -0,0 +1,25 @@
+package heranca;
+
+public class Livro extends Item {
+	private String autor;
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		if(autor.length() > 1)
+			this.autor = autor;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Livro [autor=");
+		builder.append(autor);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+}
